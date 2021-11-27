5f5dd80494928d55255e4c1634541551b58a5d16
Michael <itsmichaeldiego@gmail.com>
2021-11-26 22:51:40 -0500

    Add some TextSections

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -9,6 +9,14 @@ export function HomePage() {
     <>
       <Banner />
       <TextSection 
+        title="Power in the hands of our community." 
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip" 
+      />
+      <TextSection 
+        title="A universe of possibilities." 
+        text="$SOULYx opens a door to the future and provides options for the economic destiny of investors and holders. A whole system of virtual economy of the Suspended Soul ecosystem, with multiple diversification alternatives:" 
+      />
+      <TextSection 
         title="$SOULYx supply unlock." 
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip" 
       />

1beab3df12233b3d95e49bc9f4ec269f82a48202
Michael <itsmichaeldiego@gmail.com>
2021-12-12 17:51:27 -0500

    Improve last news section

--- src/sections/LastNews/index.tsx
+++ src/sections/LastNews/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components'
 
 import { FlipCarousel } from '../../components/FlipCarousel'
 
@@ -6,8 +7,23 @@ import { CARDS } from './data';
 
 export function LastNews() {
   return (
-    <>
+    <Wrapper>
+      <Title>LAST NEWS</Title>
       <FlipCarousel cards={CARDS} />
-    </>
+    </Wrapper>
   )
 }
+
+const Title = styled.div`
+  font-size: 238px;
+  line-height: 215px;
+  font-family: ${({ theme }) => theme.font.secondary};
+`
+
+const Wrapper = styled.div`
+  padding: ${({ theme }) => theme.spacing(20, 0, 20, 5)};
+  position: relative;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url('/images/news-door.png');
+`;

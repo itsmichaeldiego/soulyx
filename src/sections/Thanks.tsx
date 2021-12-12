54ffea3501c5c4bd12594ff36104e9f1fb7e2db7
Bruno Urrutia <brunezkey@gmail.com>
2021-12-12 18:40:17 -0300

    Adjust Thanks section

--- src/sections/Thanks.tsx
+++ src/sections/Thanks.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
-import Image from 'next/image'
-
-import { LogoSection } from '../components/LogoSection'
+import styled from 'styled-components';
+import Image from 'next/image';
 
 export function Thanks() {
   return (
-    <LogoSection>
-      <Image src="/images/thanks.svg" alt="SoulX Thannks" layout="fill" />
-    </LogoSection>
-  )
+    <SectionWrapper>
+      <LogoWrapper>
+        <Image src="/images/thanks.svg" alt="SoulX Thannks" layout="fill" />
+      </LogoWrapper>
+      <Text>THANK YOU FOR COMING THIS FAR DOWN THE ROAD WITH US. HOPE YOUR DESIRE TO GROW TOGETHER MATCHES OURS, AND WE CAN WRITE THE NEXT CHAPTER TOGETHER.</Text>
+    </SectionWrapper>
+  );
 }
+
+const SectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const LogoWrapper = styled.div`
+  max-width: 1440px;
+  min-height: 1028px;
+  position: relative;
+  background-image: url('/images/logo.png');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 35%;
+  margin-bottom: ${({ theme }) => theme.spacing(2)};
+
+  @media (max-width: 767px) {
+    max-width: 384px;
+    min-height: 254px;
+    background-size: 45%;
+  }
+`;
+
+const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: ${({ theme }) => theme.spacing(8, 6)};
+  font-weight: 300;
+  font-size: 12px;
+  max-width: 340px;
+
+  @media (max-width: 767px) {
+    padding: ${({ theme }) => theme.spacing(6, 0)};
+    max-width: 200px;
+  }
+`;

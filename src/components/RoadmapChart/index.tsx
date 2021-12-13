cd2f0184a5169437639e17557e163b1bb733f67d
Michael <itsmichaeldiego@gmail.com>
2021-12-13 16:42:21 -0500

    Fix RoadmapChart spacing

--- src/components/RoadmapChart/index.tsx
+++ src/components/RoadmapChart/index.tsx
@@ -90,7 +90,7 @@ const Wrapper = styled.div`
   max-width: 1440px;
 
   @media (min-width: 1440px) {
-    margin: 0 auto;
+    margin: ${({ theme }) => `${theme.spacing(4)} auto`};
   }
 
   @media (max-width: 767px) {

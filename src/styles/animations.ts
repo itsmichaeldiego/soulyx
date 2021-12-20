62def0b7872b0078806be7e92d73aae3b4cd7869
Michael <itsmichaeldiego@gmail.com>
2021-12-20 14:52:47 -0500

    Improve animation speed

--- src/styles/animations.ts
+++ src/styles/animations.ts
@@ -3,20 +3,20 @@ import { keyframes } from "styled-components";
 
 export const marqueeLeft = keyframes`
   0% {
-    background-position: 100vw;
+    background-position: 150vw;
   }
 
   100% {
-    background-position: -100vw;
+    background-position: -150vw;
   }
 `;
 
 export const marqueeRight = keyframes`
   0% {
-    background-position: -100vw;
+    background-position: -150vw;
   }
 
   100% {
-    background-position: 100vw;
+    background-position: 150vw;
   }
 `;

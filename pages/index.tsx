ad92084132f44e160e90eec0019a0acfb571c2c1
Michael <itsmichaeldiego@gmail.com>
2021-12-21 00:04:29 -0500

    Add analytics support

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,17 @@ const Home: NextPage = () => {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `,
+          }} />
       </Head>
       <Layout>
         <HomePage />

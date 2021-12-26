76e15bd37d487b0ea20a93670caffe71bab062ac
Michael <itsmichaeldiego@gmail.com>
2021-12-26 12:58:44 -0500

    Improve scroll logic

--- src/components/FlipCarousel.tsx
+++ src/components/FlipCarousel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components'
 
 import { FlipCard, IFlipCard } from '../components/FlipCard'
@@ -8,15 +8,25 @@ type IFlipCarouselProps = {
 }
 
 export function FlipCarousel({ cards }: IFlipCarouselProps) {
+  const scrollRef = useRef(null)
+  const handleScroll = (direction: string) => {
+    const { current } = scrollRef
+    if(direction ==='left') {
+      current?.scroll(-500, 0)
+    }
+    if(direction ==='right') {
+      current?.scroll(500, 0)
+    }
+  }
   return (
     <>
-      <Wrapper>
+      <Wrapper ref={scrollRef} id="wrapper-id">
         <Box>
           {cards.map((card, index) => <FlipCard key={card.name} card={card} index={index} />)}
         </Box>
       </Wrapper>
-      <button>Left</button>
-      <button>Right</button>
+      <button onClick={() => handleScroll('left')}>Left</button>
+      <button onClick={() => handleScroll('right')}>Right</button>
     </>
   )
 }

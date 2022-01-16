d9bbbca53562f474a199749c1a3be8929c19a5d5
Michael <itsmichaeldiego@gmail.com>
2022-01-16 00:42:26 -0600

    Add video in Intro

--- src/components/IntroLogoSection.tsx
+++ src/components/IntroLogoSection.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
-
-import { LogoSection } from './LogoSection';
-
+import Image from 'next/image';
 
 export function IntroLogoSection() {
   return (
-    <Wrapper src="/images/soulx.svg" alt="SoulX Banner" layout="fill" />
+    <Wrapper>
+      <video autoPlay muted  id="myVideo">
+        <source src="/videos/logo.mp4" type="video/mp4" />
+      </video>
+
+      <Image src="/images/soulx.svg" alt="SoulX Banner" layout="fill" />
+    </Wrapper>
   )
 }
 
-const Wrapper = styled(LogoSection)`
-  min-height: ${({ theme }) => `calc(100vh - ${theme.sizes.header} - ${theme.spacing(12)})`}; 
-  background-size: auto ${({ theme }) => `calc(100vh - ${theme.sizes.header} - ${theme.spacing(12)})`};
+const Wrapper = styled.div`
+  position: relative;
+
+  #myVideo {
+    width: 100%;
+    max-height: ${({ theme }) => `calc(100vh - ${theme.sizes.header})`}; 
+    min-width: 100%; 
+    min-height: 100%;
+  }
+
+  .content {
+    position: absolute;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+    color: #f1f1f1;
+    width: 100%;
+    padding: 20px;
+  }
 `;

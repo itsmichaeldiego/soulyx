299f8cebcec3f78649cf50ce831c83bdb8791eff
Michael <itsmichaeldiego@gmail.com>
2022-01-16 00:44:01 -0600

    Revert "Add video in Intro"
    This reverts commit d9bbbca53562f474a199749c1a3be8929c19a5d5.

--- src/components/IntroLogoSection.tsx
+++ src/components/IntroLogoSection.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import Image from 'next/image';
+
+import { LogoSection } from './LogoSection';
+
 
 export function IntroLogoSection() {
   return (
-    <Wrapper>
-      <video autoPlay muted  id="myVideo">
-        <source src="/videos/logo.mp4" type="video/mp4" />
-      </video>
-
-      <Image src="/images/soulx.svg" alt="SoulX Banner" layout="fill" />
-    </Wrapper>
+    <Wrapper src="/images/soulx.svg" alt="SoulX Banner" layout="fill" />
   )
 }
 
-const Wrapper = styled.div`
-  position: relative;
-
-  #myVideo {
-    width: 100%;
-    max-height: ${({ theme }) => `calc(100vh - ${theme.sizes.header})`}; 
-    min-width: 100%; 
-    min-height: 100%;
-  }
-
-  .content {
-    position: absolute;
-    bottom: 0;
-    background: rgba(0, 0, 0, 0.5);
-    color: #f1f1f1;
-    width: 100%;
-    padding: 20px;
-  }
+const Wrapper = styled(LogoSection)`
+  min-height: ${({ theme }) => `calc(100vh - ${theme.sizes.header} - ${theme.spacing(12)})`}; 
+  background-size: auto ${({ theme }) => `calc(100vh - ${theme.sizes.header} - ${theme.spacing(12)})`};
 `;

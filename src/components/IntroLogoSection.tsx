c73f6840e603f175bc063fe63c6c27af42423fea
Michael <itsmichaeldiego@gmail.com>
2022-01-16 21:07:34 -0600

    Move Logo video from test to main page

--- src/components/IntroLogoSection.tsx
+++ src/components/IntroLogoSection.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-
-import { LogoSection } from './LogoSection';
-
+import Image from 'next/image';
 
 export function IntroLogoSection() {
   return (
-    <Wrapper src="/images/soulx.svg" alt="SoulX Banner" layout="fill" />
+    <Wrapper>
+      <Video autoPlay muted loop>
+        <source src="/videos/logo.webm" type="video/webm" />
+      </Video>
+      <Image src="/images/soulx.svg" alt="SoulX Banner" layout="fill" />
+    </Wrapper>
   )
 }
 
-const Wrapper = styled(LogoSection)`
-  min-height: ${({ theme }) => `calc(100vh - ${theme.sizes.header} - ${theme.spacing(12)})`}; 
-  background-size: auto ${({ theme }) => `calc(100vh - ${theme.sizes.header} - ${theme.spacing(12)})`};
+const Wrapper = styled.div`
+  position: relative;
 `;
+
+const Video = styled.video`
+  width: 100%;
+  max-height: ${({ theme }) => `calc(100vh - ${theme.sizes.header})`}; 
+  min-width: 100%; 
+  min-height: 100%;
+`

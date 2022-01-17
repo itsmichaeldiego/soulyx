5f3e3d0a1d7bdadbed4f4821f0e4b8bab366cf4d
Bruno Urrutia <brunezkey@gmail.com>
2022-01-17 13:26:40 -0300

    Update metaverse text

--- src/sections/Virtual/data.ts
+++ src/sections/Virtual/data.ts
@@ -1,10 +1,10 @@
 export const VIRTUAL_CARDS = [
   {
     number: "01",
-    title: "METAVERSE",
+    title: "IMMERSIVE GALLERY",
     imageUrl: "/images/astronaut-helmet.png",
-    description: "SOULx represents all of our efforts of integrating a wide prism of possible experiences into one single immersive artistic endeavour. One which will allow all its community members, eager to take part into an integral artistic EXPERIENCE, to dive deep into a project that will make their distant dreams a virtual reality. It is the entrance to a rich universe, full of multidisciplinary and transversal possibilities that mashes together individual and collective realities, formats and timezones.",
-    descriptionShort: "SOULx represents all of our efforts of integrating a wide prism of possible experiences into one single immersive artistic endeavour. It is the entrance to a rich universe, full of multidisciplinary and transversal possibilities that mashes together individual and collective realities, formats and timezones.",
+    description: "$SOULx represents all of our efforts for integrating a wide prism of possible artistic experiences into one single immersive approach. One which will allow all its community members, eager to take part into an integral artistic EXPERIENCE, to dive deep into a project that will turn their yearnings and dreams into reality. It is the entrance to a rich universe full of multidisciplinary possibilities that will mash together individual and collective realities, as well as timezones and art formats.",
+    descriptionShort: "SOULx represents all of our efforts of integrating a wide prism of possible experiences into one single immersive approach. One which will allow all its community members, eager to take part into an integral artistic EXPERIENCE, to dive deep into a project that will turn their yearnings and dreams into reality.",
   },
   {
     number: "02",

7848b4bd84019c38922cc0643f1772a71545d4a7
Michael <itsmichaeldiego@gmail.com>
2022-02-15 00:21:04 -0600

    Set priority to Banner

--- src/components/IntroLogoSection.tsx
+++ src/components/IntroLogoSection.tsx
@@ -10,7 +10,7 @@ export function IntroLogoSection() {
   return (
     <Wrapper>
       <TokenVideo />
-      <Image src="/images/banner.svg" alt={`${TOKEN_NAME} Banner`} layout="fill" />
+      <Image src="/images/banner.svg" alt={`${TOKEN_NAME} Banner`} layout="fill" priority />
     </Wrapper>
   )
 }

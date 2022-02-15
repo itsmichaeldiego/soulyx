d1bf93c722ae0e17228020a91b18323489da67b7
Michael <itsmichaeldiego@gmail.com>
2022-02-15 02:51:40 -0600

    Improve Intro for mobile

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { IntroLogoSection } from '../components/IntroLogoSection'
 import { DiscoverSection } from '../components/DiscoverSection'
 
 export function Intro() {
   return (
-    <>
+    <Wrapper>
       <IntroLogoSection />
       <DiscoverSection text="DISCOVER THE NEW TOKEN BY SUSPENDED SOUL" />
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  min-height: ${({ theme }) => `calc(100vh - ${theme.sizes.header})`};
+`

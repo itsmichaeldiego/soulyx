d2e80ce3b17f4ab75c6fa7aea9170ab7d4212051
antonzotelo <anton@ingamana.com>
2022-03-15 10:16:40 -0300

    improve(section): Team - Wrapper margin

--- src/sections/Team/index.tsx
+++ src/sections/Team/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { FlipCarousel } from '../../components/FlipCarousel'
 
@@ -6,8 +7,12 @@ import { CARDS } from './data';
 
 export function Team() {
   return (
-    <>
+    <Wrapper>
       <FlipCarousel cards={CARDS} />
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  margin-right: ${({ theme }) => theme.spacing(-3)};
+`;
